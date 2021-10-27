0ddddff64175395467ec37ba400b6662c422006b
hat021048 <73167096+hat021048@users.noreply.github.com>
2021-10-27 15:06:56 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-basic.showIcon(IconNames.Heart)
+led.plot(2, 2)
 makerbit.connectLcd(39)
 dht11_dht22.selectTempType(tempType.celsius)
 basic.forever(function () {
@@ -9,8 +9,10 @@ basic.forever(function () {
     true,
     true
     )
-    makerbit.showStringOnLcd1602("Temp:", makerbit.position1602(LcdPosition1602.Pos1), 16)
-    makerbit.showStringOnLcd1602("" + (Math.round(dht11_dht22.readData(dataType.temperature)) + (dht11_dht22.readData(dataType.temperature) - Math.round(dht11_dht22.readData(dataType.temperature))) * 10), makerbit.position1602(LcdPosition1602.Pos7), 16)
-    makerbit.showStringOnLcd1602("Fukt:", makerbit.position1602(LcdPosition1602.Pos17), 16)
-    makerbit.showStringOnLcd1602("" + (dht11_dht22.readData(dataType.humidity)), makerbit.position1602(LcdPosition1602.Pos23), 16)
+    makerbit.showStringOnLcd1602("Temp:", makerbit.position1602(LcdPosition1602.Pos1), 5)
+    makerbit.showStringOnLcd1602("" + (Math.round(dht11_dht22.readData(dataType.temperature)) + (dht11_dht22.readData(dataType.temperature) - Math.round(dht11_dht22.readData(dataType.temperature))) * 10), makerbit.position1602(LcdPosition1602.Pos7), 4)
+    makerbit.showStringOnLcd1602("GrC", makerbit.position1602(LcdPosition1602.Pos12), 3)
+    makerbit.showStringOnLcd1602("Fukt:", makerbit.position1602(LcdPosition1602.Pos17), 5)
+    makerbit.showStringOnLcd1602("" + (dht11_dht22.readData(dataType.humidity)), makerbit.position1602(LcdPosition1602.Pos23), 2)
+    makerbit.showStringOnLcd1602("%", makerbit.position1602(LcdPosition1602.Pos26), 1)
 })
